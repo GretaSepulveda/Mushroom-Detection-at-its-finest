20dd61c1707a40699b901a8934836ac17e2f6134
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-03-11 13:32:09 -0600

    updated xaml code

---
+++ Mushroomdectector/App.xaml.cs
@@ -0,0 +1,31 @@
+
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MushroomDetector
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Bootstrapper.Init(this);
+
+            MainPage = new MainShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
